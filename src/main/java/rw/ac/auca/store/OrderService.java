9b0c96e98d14890c97234b4ce31c7806cbefad07
Shema Philbert <shemaphilbert8@gmail.com>
2026-07-27 19:10:16 +0200

    dependency injection

--- src/main/java/rw/ac/auca/store/OrderService.java
+++ src/main/java/rw/ac/auca/store/OrderService.java
@@ -1,6 +1,10 @@
 package rw.ac.auca.store;
 
 public class OrderService {
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
     private PaymentService paymentService;
     public OrderService(PaymentService paymentService){
         this.paymentService = paymentService;
