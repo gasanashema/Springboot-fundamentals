4d050a3b7b977c566298e88f5b9e32d667472d3f
Shema Philbert <shemaphilbert8@gmail.com>
2026-07-29 19:43:34 +0200

    Controlling Beans selection

--- src/main/java/rw/ac/auca/store/OrderService.java
+++ src/main/java/rw/ac/auca/store/OrderService.java
@@ -10,7 +10,7 @@ public class OrderService {
     private PaymentService paymentService;
 
 
-    public OrderService(@Qualifier('paypal') PaymentService paymentService){
+    public OrderService(@Qualifier("paypal") PaymentService paymentService){
         this.paymentService = paymentService;
     }
     public void placeOrder(){
--- src/main/java/rw/ac/auca/store/StripePaymentService.java
+++ src/main/java/rw/ac/auca/store/StripePaymentService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service("stripe")
 public class StripePaymentService implements PaymentService {
-    @Override
+
     public void processPayment(double amount){
         System.out.println("Stripe");
         System.out.println("Amount "+amount);
