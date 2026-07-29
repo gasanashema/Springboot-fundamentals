1cb2e301e0016f3f22cae9934c37d48052fe9c8a
Shema Philbert <shemaphilbert8@gmail.com>
2026-07-29 18:24:59 +0200

    Configuring Beans using annotaions

--- src/main/java/rw/ac/auca/store/OrderService.java
+++ src/main/java/rw/ac/auca/store/OrderService.java
@@ -1,5 +1,9 @@
 package rw.ac.auca.store;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
@@ -9,7 +13,4 @@ public class OrderService {
     public void placeOrder(){
         paymentService.processPayment(10);
     }
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
     }
-}
--- src/main/java/rw/ac/auca/store/PayPalPaymentService.java
+++ src/main/java/rw/ac/auca/store/PayPalPaymentService.java
@@ -1,5 +1,8 @@
 package rw.ac.auca.store;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PayPalPaymentService implements PaymentService{
     @Override
     public void processPayment(double amount) {
