2c390c267aea52931acfd0ac763f2257565c6430
Shema Philbert <shemaphilbert8@gmail.com>
2026-07-29 19:44:19 +0200

    Controlling Beans selection

--- src/main/java/rw/ac/auca/store/OrderService.java
+++ src/main/java/rw/ac/auca/store/OrderService.java
@@ -1,13 +1,11 @@
 package rw.ac.auca.store;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
-    private PaymentService paymentService;
+    private final PaymentService paymentService;
 
 
     public OrderService(@Qualifier("paypal") PaymentService paymentService){
